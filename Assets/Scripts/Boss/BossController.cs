aebd9b9a06376f4b9790699bcf900e4f742309bf
Ym <1098548238@qq.com>
2021-11-26 21:55:40 +0800

    Override Prefab & add bossController
    把prefab弄成正常的位置

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
     private float speed;
+    private Animator anim;
 
     public Collider2D swordCollider;
     
@@ -12,7 +13,7 @@ public class BossController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
